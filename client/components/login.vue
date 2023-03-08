5bb9f7aadfb930f2f8e0e43420796bbcbd48028c
Alexandr <mikhalchenkov22@gmail.com>
2023-03-08 12:06:07 +0300

    for release

---
+++ client/components/login.vue
@@ -0,0 +1,56 @@
+<style scoped>
+	#log{
+		background: blue;
+		width: 100%;
+		border-color: grey;
+	}
+	input, #log{
+		box-shadow: 0 0 10px rgba(0,0,0,0.5);
+	}
+	form{
+		width: 700px;
+	}
+	*{
+		font-size: 25px;
+	}
+	a{
+		font-size: 17px;
+	}
+</style>
+
+<template>
+	<div>
+		<form id="login_form">
+		  <div class="form-group">
+		    <label for="login">Логин</label>
+		    <input type="text" class="form-control" id="login" placeholder="Введите Логин" name="login">
+		  </div>
+		  <div class="form-group">
+		    <label for="password">Пароль</label>
+		    <input type="password" class="form-control" id="password" placeholder="Введите пароль" name="password">
+		  </div>
+		  <button type="submit" class="btn btn-primary" id="log" v-on:click="login">Войти!</button>
+		  <center><a href="http://localhost:8080/#/register" class="form-link" >Регистрация</a></center>
+		</form>
+	</div>
+</template>
+
+<script>
+	module.exports={
+		methods:{
+			login: function() {
+				console.log("login");
+				event.preventDefault();
+				let form = document.getElementById("login_form");
+				let promise = this.$auth.logIn(form.login.value, form.password.value);
+				promise.then(function(response){
+					console.log("Login success!");
+					this.$router.push("/feed");
+				}).catch(function(response){
+					console.log("Error!");
+					console.log(response);
+				});
+			}
+		}
+	}
+</script>
---
+++ client/components/register.vue
@@ -0,0 +1,64 @@
+<style>
+	#reg{
+		background: red;
+		width: 100%;
+		border-color: grey;
+	}
+	input, #reg{
+		box-shadow: 0 0 10px rgba(0,0,0,0.5);
+	}
+	form{
+		width: 700px;
+	}
+	*{
+		font-size: 25px;
+	}
+</style>
+
+<template>
+	<div>
+		<form id="reg_form">
+		  <div class="form-group" >
+		    <label for="login">Логин</label>
+		    <input type="text" class="form-control" id="login" placeholder="Введите Логин" name="login">
+		  </div>
+		  <div class="form-group">
+		    <label for="password">Пароль</label>
+		    <input type="password" class="form-control" id="password" placeholder="Введите пароль" name="password">
+		  </div>
+		  <div class="form-group">
+		    <label for="password_repeat">Повтор пароля</label>
+		    <input type="password" class="form-control" id="password_repeat" placeholder="Повторите пароль" name="repeat_password">
+		  </div>
+		  <button type="submit" class="btn btn-primary" id="reg" v-on:click="register">Зарегистрироваться!</button>
+		</form>
+	</div>
+</template>
+
+<script>
+	module.exports={
+		methods:{
+			register: function() {
+				console.log("register");
+				event.preventDefault();
+				let form = document.getElementById("reg_form");
+				let login = form.login.value;
+				let password = form.password.value;
+				let repeat_password = form.password_repeat.value;
+				if (password === repeat_password) {
+					let promise = this.$auth.register(login, password);
+					promise.then(function(response){
+						console.log("Registration success!");
+						this.$router.push("/login");
+					}).catch(function(response){
+						console.log("Error!");
+						console.log(response);
+					});
+				}else{
+					console.log("Пароли не совпадают");
+                    alert('Пароли не совпадают')
+				}
+			}
+		}
+	}
+</script>
